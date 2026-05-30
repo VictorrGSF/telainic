635464bd1cdcea6af22bb2aca2beb0b301709cd7
Victor <victorgabriel4973@gmail.com>
2026-05-30 09:50:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/telainic/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.telainic
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val bt_inicial = findViewById<Button>(R.id.btn_inicial)
+
+        bt_inicial.setOnClickListener {
+
+            val intent = Intent(this, segundaTela::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+            }
+        }
+    }
---
+++ app/src/main/java/com/example/telainic/segundaTela.kt
@@ -0,0 +1,57 @@
+package com.example.telainic
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+
+
+
+class segundaTela : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda_tela)
+        val cadastrar = findViewById<Button>(R.id.cadastrar)
+        val campoNome = findViewById<EditText>(R.id.NomeTEXT)
+        val campoEmail = findViewById<EditText>(R.id.campoEmail)
+        val btn_voltar = findViewById<Button>(R.id.btn_voltar)
+
+
+        btn_voltar.setOnClickListener {
+            finish()
+        }
+       cadastrar.setOnClickListener {
+           val nome = campoNome.text.toString()
+           val email = campoEmail.text.toString()
+
+           AlertDialog.Builder(this)
+               .setTitle("Cadastro realizado")
+               .setMessage(
+                   "Cadastro realizado com sucesso!\n\n" +
+                           "Nome: $nome\n" +
+                           "Email: $email\n\n" +
+                           "Um email de confirmação será enviado."
+               )
+               .setPositiveButton("OK") { _, _ ->
+
+                   finish()}
+               .show()
+
+
+       }
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
