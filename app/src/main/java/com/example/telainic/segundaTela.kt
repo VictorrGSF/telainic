03f1669e7b1ef2bb25f6445228c4e06597eda55a
Victor <victorgabriel4973@gmail.com>
2026-06-03 11:49:50 -0300

    Foi feita uma alteraçãp no evento do POP-UP após o clique redireciona para uma terceira tela , que terá campos para serem preenchidos pelo usuário referente ao tipo da consulta desejada

--- app/src/main/java/com/example/telainic/segundaTela.kt
+++ app/src/main/java/com/example/telainic/segundaTela.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
+import android.content.Intent
 
 
 
@@ -39,7 +40,9 @@ class segundaTela : AppCompatActivity() {
                )
                .setPositiveButton("OK") { _, _ ->
 
-                   finish()}
+                   val intent = Intent (this,telaLogada::class.java)
+                   startActivity(intent)
+               }
                .show()
 
 
